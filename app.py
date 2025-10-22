821f26dd48455306b084fe38d03cdea2fc9d22de
Roberto Montoya <montoya.bueno@gmail.com>
2025-10-22 14:51:17 -0600

    Webhook2.11.7 Listas Adictt

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def sand_message_whatsapp(txt_message, to_number):
                 }
             }
         }
-    elif "btncoadictt" in txt_message:
+    elif "adictt" in txt_message:
         data = {
             "messaging_product": "whatsapp",
             "recipient_type": "individual",
@@ -176,7 +176,7 @@ def sand_message_whatsapp(txt_message, to_number):
                 },
                 "action": {
                     "button": "Ver opciones",
-                    "sections": [
+                    "section": [
                         {
                             "title": "Boutiques Adictt",
                             "rows": [
